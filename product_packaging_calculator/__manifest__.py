1bd81d45ef88dec0e39cbba4847cb517d07ab3d4
Sébastien Alix <sebastien.alix@camptocamp.com>
2025-03-17 15:15:51 +0100

    [MOV] stock_packaging_calculator: renamed to product_packaging_calculator
    And moved from OCA/stock-logistics-warehouse where it was already migrated.
    
    As this module doesn't depend on `stock`, there is no need for this module to:
    - be hosted in a stock-logistics-* repository
    - be named with prefix `stock_`
    
    `stock_packaging_calculator` will be transformed into a transitional
    module to install the new `product_packaging_calculator`.

--- product_packaging_calculator/__manifest__.py
+++ product_packaging_calculator/__manifest__.py
@@ -1,15 +1,21 @@
 # Copyright 2020 Camptocamp SA
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl)
 {
-    "name": "Stock packaging calculator",
+    "name": "Product packaging calculator",
     "summary": "Compute product quantity to pick by packaging",
     "version": "18.0.1.0.0",
     "development_status": "Beta",
-    "category": "Warehouse Management",
+    "category": "Product",
     "website": "https://github.com/OCA/product-attribute",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "application": False,
     "installable": True,
     "depends": ["product"],
+    "external_dependencies": {
+        "python": [
+            "openupgradelib",
+        ],
+    },
+    "pre_init_hook": "pre_init_hook",
 }
