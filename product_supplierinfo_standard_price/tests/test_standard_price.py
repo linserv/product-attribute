ae3ca62d11dd8e2965704eebc95acffeee95c5e4
Quentin Dupont <quentin.dupont@grap.coop>
2025-04-10 11:18:57 +0200

    IMP test and coverage

--- product_supplierinfo_standard_price/tests/test_standard_price.py
+++ product_supplierinfo_standard_price/tests/test_standard_price.py
@@ -16,7 +16,19 @@ class TestStandardPrice(TransactionCase):
         # Others
         self.uom_dozen = self.env.ref("uom.product_uom_dozen")
 
-    def test_01_set_new_price(self):
+    def test_00__get_standard_price_fields(self):
+        res = self.office_chair_suppinfo._get_standard_price_fields()
+        self.assertNotIn("discount", res)
+        self.assertIn("price", res)
+
+    def test_01_compute_theoritical_standard_price_no_discount(self):
+        self.office_chair_suppinfo._compute_theoritical_standard_price()
+        self.assertEqual(
+            self.office_chair_suppinfo.theoritical_standard_price,
+            self.office_chair_suppinfo_price,
+        )
+
+    def test_02_set_new_price(self):
         # Changing Standard price with supplier info
         self.office_chair_suppinfo.set_product_standard_price_from_supplierinfo()
         self.assertEqual(
@@ -24,8 +36,8 @@ class TestStandardPrice(TransactionCase):
             self.office_chair.standard_price,
         )
 
-    # Test if product standard price is well calculated with purchase uom
-    def test_02_change_po_uom(self):
+    def test_03_change_po_uom(self):
+        # Test if product standard price is well calculated with purchase uom
         self.office_chair.uom_po_id = self.uom_dozen
         self.office_chair_suppinfo.set_product_standard_price_from_supplierinfo()
         self.assertEqual(
